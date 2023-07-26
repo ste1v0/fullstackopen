1fb5615155156e006c75dc9032b880f01586a2e4
ste1v0 <tyotarjous@yandex.ru>
2023-07-27 00:32:29 +0200

    feat filter

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+
+  const [value, setValue] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    console.log('getting countries list')
+
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        const countriesArray = response.data.map(x => x.name.common)
+        setCountries(countriesArray)
+        console.log(countriesArray)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log(`effect is triggered, country value is ${value}`)
+
+    if (value) {
+      axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${value}`)
+      .then(response => {
+        return response.data.name.common
+      })
+      .catch(error => {
+        console.log(`Country not found`)
+      })
+    }
+  }, [value])
+
+    const handleInputChange = (event) => {
+      console.log(`value has changed`)
+      setValue(event.target.value)
+    }
+
+    const filteredCountries = countries.filter(x => x.includes(value))
+
+    return (
+      <div className="App">
+        find countries
+        <form>
+          <input value={value} onChange={handleInputChange}></input>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {filteredCountries.length > 10
+            ? `Too many matches, specify another filter`
+            : countries.filter(x => x.includes(value))
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    )
+  }
 
-export default App;
+export default App
