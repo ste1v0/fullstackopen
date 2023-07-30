305f579f15a158c248f923dbae55eb0daccdc99f
ste1v0 <tyotarjous@yandex.ru>
2023-07-30 19:33:04 +0200

    fix partial match feature

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,14 +1,16 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './index.css'
 
 const App = () => {
 
   const [value, setValue] = useState('')
   const [countries, setCountries] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState(null)
 
   useEffect(() => {
     console.log('getting countries list')
-
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then(response => {
@@ -16,42 +18,67 @@ const App = () => {
         setCountries(countriesArray)
         console.log(countriesArray)
       })
+      .catch(error => {
+        console.log(`Error: ${error}`)
+      })
   }, [])
 
   useEffect(() => {
     console.log(`effect is triggered, country value is ${value}`)
-
     if (value) {
-      axios
-      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${value}`)
-      .then(response => {
-        return response.data.name.common
-      })
-      .catch(error => {
-        console.log(`Country not found`)
-      })
+      const filteredCountries = countries.filter(x => x.toLowerCase().includes(value.toLowerCase()))
+      if (filteredCountries.length === 1) {
+        setLoading(true)
+        axios
+          .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${filteredCountries[0]}`)
+          .then(response => {
+            const countryData = response.data
+            setData(countryData)
+            console.log(countryData)
+            setLoading(false)
+          })
+          .catch(error => {
+            setLoading(false)
+            setData(null)
+            console.log(`Country not found`)
+          })
+      } else {
+        setData(null)
+      }
+      
     }
-  }, [value])
+  }, [value, countries])
 
     const handleInputChange = (event) => {
       console.log(`value has changed`)
       setValue(event.target.value)
     }
 
-    const filteredCountries = countries.filter(x => x.includes(value))
-
+    const filteredCountries = countries.filter(x => x.toLowerCase().includes(value.toLowerCase()))
+    
     return (
       <div className="App">
-        find countries
+        <h1>Find countries</h1>
         <form>
           <input value={value} onChange={handleInputChange}></input>
         </form>
-        <p>
-          {filteredCountries.length > 10
-            ? `Too many matches, specify another filter`
-            : filteredCountries.map((x) => <div>{x}</div>)
-          }
-        </p>
+        {loading && <p>Loading...</p>}
+        {filteredCountries.length > 10
+          ? `Too many matches, specify another filter`
+          : filteredCountries.map(x => <div key={x}> <li>{x}</li></div>)
+        }     
+        {data !== null && (
+          <div>
+              <h2>{data.name.common}</h2>
+              <p>capital {data.capital}<br></br>
+              area {data.area}</p>
+              <p><b>languages:</b></p>
+              <ul>
+                {Object.values(data.languages).map(x => <li key={x}>{x}</li>)}
+              </ul>
+              <img width="250" height="250" alt="country-flag" src={data.flags.svg}></img>
+          </div>
+          )}
       </div>
     )
   }
