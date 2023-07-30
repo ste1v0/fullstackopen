4aaa3d59a9c0c53729b7651853afc6cd2f9a7a31
ste1v0 <tyotarjous@yandex.ru>
2023-07-30 22:45:00 +0200

    refactor

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -9,24 +9,26 @@ const App = () => {
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState(null)
 
+  // Fetching countries list
+
   useEffect(() => {
-    console.log('getting countries list')
     axios
       .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
       .then(response => {
         const countriesArray = response.data.map(x => x.name.common)
         setCountries(countriesArray)
-        console.log(countriesArray)
       })
       .catch(error => {
         console.log(`Error: ${error}`)
       })
   }, [])
 
+  const filteredCountries = countries.filter(x => x.toLowerCase().includes(value.toLowerCase()))
+
   useEffect(() => {
-    console.log(`effect is triggered, country value is ${value}`)
+    // if 'value' has changed, find similar countries
     if (value) {
-      const filteredCountries = countries.filter(x => x.toLowerCase().includes(value.toLowerCase()))
+        // if there is only one match, fetch additional details
       if (filteredCountries.length === 1) {
         setLoading(true)
         axios
@@ -34,27 +36,22 @@ const App = () => {
           .then(response => {
             const countryData = response.data
             setData(countryData)
-            console.log(countryData)
             setLoading(false)
           })
           .catch(error => {
-            setLoading(false)
             setData(null)
-            console.log(`Country not found`)
+            setLoading(false)
           })
       } else {
+        // More than 1 match? Leave blank
         setData(null)
       }
-      
     }
   }, [value, countries])
 
     const handleInputChange = (event) => {
-      console.log(`value has changed`)
       setValue(event.target.value)
     }
-
-    const filteredCountries = countries.filter(x => x.toLowerCase().includes(value.toLowerCase()))
     
     return (
       <div className="App">
@@ -65,20 +62,19 @@ const App = () => {
         {loading && <p>Loading...</p>}
         {filteredCountries.length > 10
           ? `Too many matches, specify another filter`
-          : filteredCountries.map(x => <div key={x}> <li>{x}</li></div>)
+          : data === null
+            ? filteredCountries.map(x => <div key={x}> <li>{x}</li></div>)
+            : <div>
+                <h2>{data.name.common}</h2>
+                <p>capital {data.capital}<br></br>
+                area {data.area}</p>
+                <p><b>languages:</b></p>
+                <ul>
+                  {Object.values(data.languages).map(x => <li key={x}>{x}</li>)}
+                </ul>
+                <img width="250" height="250" alt="country-flag" src={data.flags.svg}></img>
+            </div>
         }     
-        {data !== null && (
-          <div>
-              <h2>{data.name.common}</h2>
-              <p>capital {data.capital}<br></br>
-              area {data.area}</p>
-              <p><b>languages:</b></p>
-              <ul>
-                {Object.values(data.languages).map(x => <li key={x}>{x}</li>)}
-              </ul>
-              <img width="250" height="250" alt="country-flag" src={data.flags.svg}></img>
-          </div>
-          )}
       </div>
     )
   }
