13a7d329215342dc1ceef041940cb52a44fd6fe0
ste1v0 <tyotarjous@yandex.ru>
2023-08-01 00:40:45 +0200

    feat add temp/windspeed data

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,12 +2,16 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './index.css'
 
+const weatherAPI = process.env.REACT_APP_WEATHER_API
+const picURL = `https://openweathermap.org/img/wn`
+
 const App = () => {
 
   const [value, setValue] = useState('')
   const [countries, setCountries] = useState([])
-  const [loading, setLoading] = useState(false)
   const [data, setData] = useState(null)
+  const [weatherData, setWeatherData] = useState(null)
+  const [picture, setPicture] = useState(null)
 
   // Fetching countries list
 
@@ -30,18 +34,26 @@ const App = () => {
     if (value) {
         // if there is only one match, fetch additional details
       if (filteredCountries.length === 1) {
-        setLoading(true)
         axios
           .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${filteredCountries[0]}`)
           .then(response => {
             const countryData = response.data
             setData(countryData)
-            setLoading(false)
           })
           .catch(error => {
             setData(null)
-            setLoading(false)
           })
+          axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${filteredCountries[0]}&appid=${weatherAPI}`)
+            .then(response => {
+              const weather = response.data
+              setWeatherData(weather)
+              setPicture(weather.weather[0].icon)
+              console.log(picture)
+            })
+            .catch(error => {
+              setWeatherData(null)
+            })
       } else {
         // More than 1 match? Leave blank
         setData(null)
@@ -63,21 +75,24 @@ const App = () => {
         <form>
           <input value={value} onChange={handleInputChange}></input>
         </form>
-        {loading && <p>Loading...</p>}
         {filteredCountries.length > 10
           ? `Too many matches, specify another filter`
-          : data === null
-            ? filteredCountries.map(x => <div key={x}> <li>{x} <button onClick={() => handleButtonClick(x)}>show</button></li> </div>)
-            : <div>
-                <h2>{data.name.common}</h2>
-                <p>capital {data.capital}<br></br>
-                area {data.area}</p>
-                <p><b>languages:</b></p>
-                <ul>
-                  {Object.values(data.languages).map(x => <li key={x}>{x}</li>)}
-                </ul>
-                <img width="250" height="250" alt="country-flag" src={data.flags.svg}></img>
-            </div>
+          : data !== null && weatherData !== null && picture !== null
+            ? <div>
+            <h2>{data.name.common}</h2>
+            <p>capital {data.capital}<br></br>
+            area {data.area}</p>
+            <p><b>languages:</b></p>
+            <ul>
+              {Object.values(data.languages).map(x => <li key={x}>{x}</li>)}
+            </ul>
+            <h2>Weather in {data.name.common}</h2>
+            <p>temperature {Math.round(weatherData.main.temp - 273.15)} celsius</p>
+            <img alt='weather' src={`${picURL}/${picture}@2x.png`}></img>
+            <p>wind {weatherData.wind.speed} m/s</p>
+            <img width="250" height="250" alt="country-flag" src={data.flags.svg}></img>
+        </div> 
+            : filteredCountries.map(x => <div key={x}> <li>{x} <button onClick={() => handleButtonClick(x)}>show</button></li> </div>)
         }     
       </div>
     )
