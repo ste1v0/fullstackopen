1a8ff27b12e5f84bc9826cd93510125039062275
ste1v0 <tyotarjous@yandex.ru>
2023-08-19 12:54:36 +0200

    add steps 1-2

--- part3/index.js
+++ part3/index.js
@@ -3,76 +3,40 @@ const express = require('express')
 const app = express()
 
 let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
     },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
     },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
     }
 ]
 
 app.get('/', (request, response) => {
-    response.send('Hello World!')
+    response.send('App is working!')
 })
 
-app.get('/api/notes', (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.send(notes)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    note
-     ? response.json(note)
-     : response.status(404).end() 
+app.get('/info', (request, response) => {
+    const dateTimeObject = new Date();
+    response.send(`Phonebook has info for ${notes.length} people<br/><br/>${dateTimeObject}`)
 })
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-})
-
-app.use(express.json())
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId()
-    }
-
-
-notes = notes.concat(note)
-})
-
-
 
 const PORT = 3001
 
