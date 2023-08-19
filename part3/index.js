8b63923c340cecd44be92424aa444726d2ea645b
ste1v0 <tyotarjous@yandex.ru>
2023-08-19 12:40:29 +0200

    init part3

--- part3/index.js
+++ part3/index.js
@@ -42,7 +42,35 @@ app.delete('/api/notes/:id', (request, response) => {
     notes = notes.filter(note => note.id !== id)
   
     response.status(204).end()
-  })
+})
+
+app.use(express.json())
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    }
+
+
+notes = notes.concat(note)
+})
 
 
 
