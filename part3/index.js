5bffbbae08af363741db7f657f5817fc602b4625
ste1v0 <tyotarjous@yandex.ru>
2023-08-19 14:01:22 +0200

    add step 4

--- part3/index.js
+++ part3/index.js
@@ -47,6 +47,13 @@ app.get('/api/persons/:id', (request, response) => {
         : response.status(404).end()
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(n => n.id !== id)
+    
+    response.status(204).end()
+})
+
 const PORT = 3001
 
 app.listen(PORT, () => {
